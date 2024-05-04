6db4f3f81e86496e4d6a6907c286b8f52236fc41
marko154 <marko.gartnar777@gmail.com>
2024-05-04 23:38:00 +0200

    telescope git branches

--- lua/kickstart/plugins/neogit.lua
+++ lua/kickstart/plugins/neogit.lua
@@ -8,5 +8,8 @@ return {
     -- Only one of these is needed, not both.
     'nvim-telescope/telescope.nvim', -- optional
   },
-  config = true,
+  config = function()
+    vim.keymap.set('n', '<leader>fb', ':Telescope git_branches<CR>', { desc = '[F]ind [B]ranch' })
+    require('neogit').setup {}
+  end,
 }
