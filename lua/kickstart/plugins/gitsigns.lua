440e16d77c89d68e8f6d4bfe047bae94778c2128
m.gartnar <m.gartnar@sportradar.com>
2024-05-26 16:32:00 +0200

    enable line blame with gitsigns

--- lua/kickstart/plugins/gitsigns.lua
+++ lua/kickstart/plugins/gitsigns.lua
@@ -5,6 +5,10 @@ return {
   {
     'lewis6991/gitsigns.nvim',
     opts = {
+      current_line_blame = true,
+      current_line_blame_opts = {
+        delay = 300,
+      },
       on_attach = function(bufnr)
         local gitsigns = require 'gitsigns'
 
